47a312988c24342a848cd15a28d4ac1628906285
Vinod <vinod@7unique.in>
2026-01-19 11:52:28 +0530

    updated homepage

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <>
+       <div className="hero-section">
+         <h1>Welcome to Our Website</h1>
+         <p>Your journey to excellence starts here.</p>
+       </div> 
+    </>
+    );
+};
+export default Hero;
